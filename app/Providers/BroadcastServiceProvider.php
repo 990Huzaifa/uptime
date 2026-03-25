28170f6991a1d80ecfd3ec0239bfa0adde6e5000
990Huzaifa <106827131+990Huzaifa@users.noreply.github.com>
2026-03-25 16:40:19 +0500

    minor changes

--- app/Providers/BroadcastServiceProvider.php
+++ app/Providers/BroadcastServiceProvider.php
@@ -12,7 +12,11 @@ class BroadcastServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-
+        Broadcast::routes(['middleware' => ['auth:sanctum']]);
+        
         require base_path('routes/channels.php');
+        
+        \Log::info('Broadcast route hit test');
+
     }
 }
